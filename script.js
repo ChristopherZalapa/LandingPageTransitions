a849ada4be3701ba3b5e8c4d86f4dc9aab63fd21
ChristopherZalapa <chriszalapa@gmail.com>
2021-01-19 12:51:29 -0800

    Image Selectors

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let header = document.querySelector('.header');
 let site = document.querySelector('.site');
 let cta = document.querySelector('.cta');
 
+let imageOverLay = document.querySelector('.image-container').children[0];
+let mainImage = document.querySelector('.image-container').children[1];
+
